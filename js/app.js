1ddfeddee82123f362bb11a5fdade6bc785e733e
Andy Stevens <57344820+apstevens@users.noreply.github.com>
2020-11-27 16:42:49 +0000

    Updated styles and js

--- js/app.js
+++ js/app.js
@@ -5,3 +5,18 @@ function overlay() {
 }
 
 setTimeout(overlay, 6000);
+
+let i = 0;
+let text = 'Predict the future by to creating it!';
+let speed = 50;
+
+
+window.onload = function slideShow() {
+  if (i < text.length) {
+    document.getElementById('quote').innerHTML += text.charAt(i);
+    i++;
+    setTimeout( slideShow, speed );
+  }
+
+
+}
